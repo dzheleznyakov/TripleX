63f341a5b575f435ccedc1e0b8e0d74b7477c47a
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-03-15 10:19:33 +0000

    read user's guesses

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,21 +5,25 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
     std::cout << "Enter the correct code to continue..." << std::endl;
 
-    int CodeA = 4;
-    int CodeB = 3;
-    int CodeC = 2;
+    int CodeA {4};
+    int CodeB {3};
+    int CodeC {2};
     
-    int CodeSum = CodeA + CodeB + CodeC;
-    int CodeProduct = CodeA * CodeB * CodeC;
+    int CodeSum {CodeA + CodeB + CodeC};
+    int CodeProduct {CodeA * CodeB * CodeC};
 
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add up to " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give " << CodeProduct << std::endl;
 
-    int PlayerGuess;
-    std::cin >> PlayerGuess;
-    std::cout << "You entered: " << PlayerGuess;
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum {GuessA + GuessB + GuessC};
+    int GuessProduct {GuessA + GuessB + GuessC};
 
     std::cout << std::endl;
     return 0;
